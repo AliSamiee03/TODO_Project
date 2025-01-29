1b6078ba6ec43c651b9925b2edf7e29c2e021d57
Ali Samiee <alisamiee844@gmail.com>
2025-01-29 11:58:32 +0330

    CRUD views were created for the user model

--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,39 @@
+from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
-from rest_framework import status
+from rest_framework import status, generics
+from rest_framework.views import APIView
 
+class CreateUserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class ListUsersView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(instance=users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class DetailUserView(APIView):
+    def get(self, request, pk):
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(instance=user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdateUserView(APIView):
+    def put(self, request, pk):
+        user = User.objects.get(pk=pk)
+        serializer_data = UserSerializer(instance=user, data=request.data, partial=True)
+        if serializer_data.is_valid():
+            serializer_data.save()
+            return Response(serializer_data.data, status=status.HTTP_200_OK)
+        return Response(serializer_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DeleteUserView(APIView):
+    def delete(self, request, pk):
+        user = User.objects.get(pk=pk)
+        user.delete()
