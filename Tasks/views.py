f088141112a5b2eb76fd23ce52dd827b080e9882
Ali Samiee <alisamiee844@gmail.com>
2025-01-29 16:26:06 +0330

    Custom permission was applied to task views

--- Tasks/views.py
+++ Tasks/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from permissions import IsOwnerTaskOrAdmin
 
 class ListTasks(APIView):
     permission_classes = [IsAdminUser, IsAuthenticated]
@@ -15,7 +16,7 @@ class ListTasks(APIView):
 
 
 class DetailTask(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerTaskOrAdmin]
 
     def get(self, request, pk):
         task = Task.objects.get(pk=pk)
@@ -23,7 +24,7 @@ class DetailTask(APIView):
         return Response(serializer_data.data, status=status.HTTP_200_OK)
 
 class CreateTask(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerTaskOrAdmin]
 
     def post(self, request):
         serializer_data = TaskSerializer(data=request.data)
@@ -34,7 +35,7 @@ class CreateTask(APIView):
         return Response(serializer_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UpdateTask(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerTaskOrAdmin]
 
     def put(self, request, pk):
         task = Task.objects.get(pk=pk)
@@ -46,7 +47,7 @@ class UpdateTask(APIView):
         return Response(serializer_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class DeleteTask(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerTaskOrAdmin]
 
     def delete(self, request, pk):
         task = Task.objects.get(pk=pk)
