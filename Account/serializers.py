a67c49649abe73ddf87221ca87fd30f336c1e751
Ali Samiee <alisamiee844@gmail.com>
2025-01-29 12:36:10 +0330

    The serializer app account has been updated

--- Account/serializers.py
+++ Account/serializers.py
@@ -5,4 +5,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-
+        extra_kwargs = {
+            'password': {'write_only': True, 'min_length': 5, 'max_length': 20},
+            'email': {'required': True},
+            'username': {'required': True},
+            'phone': {'required': True},
+        }
